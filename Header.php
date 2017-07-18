f086fe379be5cab2ad41f1ad378e5b1520937f0a
Luis Valdez <you@example.com>
2017-07-18 10:46:27 -0600

    Cambios minimos a script y header

--- Header.php
+++ Header.php
@@ -18,13 +18,26 @@ $Header = '
 <link href="../vendors/nprogress/nprogress.css" rel="stylesheet">
 <!-- iCheck -->
 <link href="../vendors/iCheck/skins/flat/green.css" rel="stylesheet">
-
+<!-- bootstrap-wysiwyg -->
+<link href="../vendors/google-code-prettify/bin/prettify.min.css" rel="stylesheet">
 <!-- bootstrap-progressbar -->
 <link href="../vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
 <!-- JQVMap -->
 <link href="../vendors/jqvmap/dist/jqvmap.min.css" rel="stylesheet"/>
+<!-- Select2 -->
+<link href="../vendors/select2/dist/css/select2.min.css" rel="stylesheet">
+<!-- Switchery -->
+<link href="../vendors/switchery/dist/switchery.min.css" rel="stylesheet">
+<!-- starrr -->
+<link href="../vendors/starrr/dist/starrr.css" rel="stylesheet">
 <!-- bootstrap-daterangepicker -->
 <link href="../vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
+<!-- PNotify -->
+<link href="../vendors/pnotify/dist/pnotify.css" rel="stylesheet">
+<link href="../vendors/pnotify/dist/pnotify.buttons.css" rel="stylesheet">
+<link href="../vendors/pnotify/dist/pnotify.nonblock.css" rel="stylesheet">
+<!-- bootstrap-datetimepicker -->
+<link href="../vendors/bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.css" rel="stylesheet">
 
 <!-- Custom Theme Style -->
 <link href="../build/css/custom.min.css" rel="stylesheet">
